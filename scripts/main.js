8da7d4d0efde94f2a3fe39f19753afc85bb6baae
Aymane <erenhaydar888@gmail.com>
2025-11-06 16:58:02 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+const asideBar = document.getElementById('asideBar');
+const OpenAsideBar = document.getElementById('OpenAsideBar');
+const CloseAsideBar = document.getElementById('CloseAsideBar');
+const overly = document.getElementById('overly');
+const asideLink = document.querySelectorAll('.asideLink');
+const navUl = document.querySelector('.navUl');
+const navbar = document.querySelector('.navbar');
+
+
+
+asideLink.forEach((el)=>{
+    el.addEventListener("click",()=>{
+         asideBar.classList.remove('right-0');
+    asideBar.classList.add('-right-96');
+    overly.classList.add('hidden');
+    });
+});
+
+OpenAsideBar.addEventListener('click',()=>{
+    asideBar.classList.remove('-right-96');
+    asideBar.classList.add('right-0');
+    overly.classList.remove('hidden');
+});
+CloseAsideBar.addEventListener('click',()=>{
+    asideBar.classList.remove('right-0');
+    asideBar.classList.add('-right-96');
+    overly.classList.add('hidden');
+});
+overly.addEventListener('click',()=>{
+    asideBar.classList.remove('right-0');
+    asideBar.classList.add('-right-96');
+    overly.classList.add('hidden');
+});
+
+
+window.addEventListener('scroll',()=>{
+    if(scrollY > 50){
+       navUl.classList.remove('bg-white/20','border-gray-400','border','backdrop-blur-lg');
+       navbar.classList.add('bg-white/10','backdrop-blur-lg');
+    }else{
+       navUl.classList.add('bg-white/20','border-gray-400','border','backdrop-blur-lg');
+       navbar.classList.remove('bg-white/10','backdrop-blur-lg');
+    }
+})
