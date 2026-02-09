50c69312a0d1e9fd0ae453e4d5e4b9ee923976c9
Aymane <erenhaydar888@gmail.com>
2026-02-09 22:19:10 +0100

    change ui of sidebar

---
+++ scripts/data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        img : "assets/img/project_quize.webp",
+        title : "Quiz App",
+        description : "a simple quiz app UI created using html tailwind JavaScript.",
+        type : "frontend" ,
+        demo_link : "https://aymoxd.github.io/quiz/",
+        repo_link : "https://github.com/aymoxd/quiz.git"
+    } ,
+      {
+        img : "assets/img/project_dashboard.webp",
+        title : "Dashboard",
+        description : "A simple and clean dashboard interface built using HTML, Tailwind CSS, and JavaScript.",
+        type : "frontend",
+        demo_link : "https://aymoxd.github.io/dashboard/",
+        repo_link : "https://github.com/aymoxd/dashboard.git"
+    } ,
+        {
+        img : "assets/img/project_portproject.webp",
+        title : "Port project",
+        description : "A collection of my JavaScript projects focused on clean, functional logic without heavy styling",
+        type : "frontend",
+        demo_link : "https://aymoxd.github.io/portProject/",
+        repo_link : "https://github.com/aymoxd/portProject.git"
+    } 
+];
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+import { data } from "./data.js";
+
 const asideBar = document.getElementById('asideBar');
 const OpenAsideBar = document.getElementById('OpenAsideBar');
 const CloseAsideBar = document.getElementById('CloseAsideBar');
@@ -8,6 +10,7 @@ const navbar = document.querySelector('.navbar');
 const form = document.getElementById('contactForm');
 const  darkMode = document.getElementById('darkMode');
 const upbtn = document.getElementById('upbtn');
+const links = document.querySelector('.links');
 
 
 upbtn.addEventListener('click', () => {
@@ -27,25 +30,50 @@ darkMode.addEventListener('click',()=>{
 
 asideLink.forEach((el)=>{
     el.addEventListener("click",()=>{
-         asideBar.classList.remove('right-0');
-    asideBar.classList.add('-right-96');
+    links.classList.add('hidden');
+    CloseAsideBar.classList.add('hidden');
+    links.classList.add('h-0');
+    links.classList.add('opacity-0');
+    overly.classList.add('hidden');
+    links.classList.add('pointer-events-none');
+    links.classList.remove('h-[400px]');
+    CloseAsideBar.classList.add('hidden');
+    overly.classList.remove('hidden');
     overly.classList.add('hidden');
     });
 });
 
 OpenAsideBar.addEventListener('click',()=>{
-    asideBar.classList.remove('-right-96');
-    asideBar.classList.add('right-0');
+    links.classList.remove('hidden');
+    CloseAsideBar.classList.remove('hidden');
+    links.classList.remove('h-0');
+    links.classList.remove('opacity-0');
     overly.classList.remove('hidden');
+    links.classList.remove('pointer-events-none');
+    links.classList.add('h-[400px]');
+    CloseAsideBar.classList.remove('hidden');
 });
+
 CloseAsideBar.addEventListener('click',()=>{
-    asideBar.classList.remove('right-0');
-    asideBar.classList.add('-right-96');
+    links.classList.add('hidden');
+    CloseAsideBar.classList.add('hidden');
+    links.classList.add('h-0');
+    links.classList.add('opacity-0');
+    overly.classList.add('hidden');
+    links.classList.add('pointer-events-none');
+    links.classList.remove('h-[400px]');
+    CloseAsideBar.classList.add('hidden');
     overly.classList.add('hidden');
 });
 overly.addEventListener('click',()=>{
-    asideBar.classList.remove('right-0');
-    asideBar.classList.add('-right-96');
+    CloseAsideBar.classList.add('hidden');
+    links.classList.add('h-0');
+    links.classList.add('opacity-0');
+    overly.classList.add('hidden');
+    links.classList.add('pointer-events-none');
+    links.classList.remove('h-[400px]');
+    CloseAsideBar.classList.add('hidden');
+    overly.classList.remove('hidden');
     overly.classList.add('hidden');
 });
 
@@ -98,3 +126,33 @@ function sendMsg(){
         });
     }
 }
+
+const project_area = document.getElementById('project_area');
+
+
+data.forEach((project)=> {
+
+
+project_area.innerHTML += `
+<div data-aos="zoom-in" data-aos-delay="400" class="bg-white dark:bg-gray-800 group p-2 cursor-pointer transition-all duration-300 hover:-translate-y-1 border border-gray-300 dark:border-gray-500 rounded-xl">
+                  <div class="bg-rose-400 rounded-xl overflow-hidden">
+                       <img class="w-full group-hover:scale-105 transition-all duration-300  rounded-t-xl sm:h-52 object-cover" src="${project.img}" alt="${project.title}" loading="lazy">
+                  </div>
+                  <div class="px-3 space-y-3 rounded-b-xl pb-3 mt-3">
+                       <div class="flex items-center justify-between">
+                       <h1 class="text-gray-800 dark:text-gray-200 text-lg">${project.title}</h1>
+                        <span class="text-xs px-3 py-1 bg-blue-100 text-blue-700 rounded-full">${project.type}</span>
+                       </div>
+                       <p class="text-sm text-gray-600 dark:text-gray-400 h-20">${project.description}</p>
+                       <div class="flex gap-3 text-white">
+                         <a href="${project.demo_link}" href class="bg-gradient-to-t from-blue-500 to-blue-400 dark:bg-gradient-to-t dark:from-rose-700 dark:to-rose-600 flex items-center justify-center text-nowrap cursor-pointer px-3 py-1.5 rounded-xl w-full" target="_blank" rel="noopener noreferrer">Demo <i class="ri-arrow-right-up-line text-xl"></i></a>
+                         <a href="${project.repo_link}" class="bg-gray-800 hover:bg-black dark:hover:bg-white dark:hover:text-black transition-all duration-300 cursor-pointer flex items-center justify-center w-12 rounded-full" target="_blank" rel="noopener noreferrer"><i class="ri-github-line text-xl"></i></a>
+                       </div>
+                       
+                  </div>
+                   
+                 </div>
+
+`;
+
+});
